cdc38ba6519f6e6fc3025e7fe0b11b74c21ecb02
webyeah123 <93124315+webyeah123@users.noreply.github.com>
2021-12-19 18:26:19 +0800

    Update zqydCOOKIE.js

--- Task/zqydCOOKIE.js
+++ Task/zqydCOOKIE.js
@@ -21,6 +21,36 @@ module.exports = {
       "val": "0",
       "type": "number",
       "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydnotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "zqydnotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "zqydMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
     }],
     "repo": "https://gitee.com/tom210120/tom/raw/master/ZQ/zq.js",
     "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
