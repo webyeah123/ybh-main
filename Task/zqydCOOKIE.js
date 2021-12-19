0b2e97ff5d0a4b6787135b6db9ace930b4b33d82
webyeah123 <93124315+webyeah123@users.noreply.github.com>
2021-12-19 18:16:23 +0800

    Update zqydCOOKIE.js

--- Task/zqydCOOKIE.js
+++ Task/zqydCOOKIE.js
@@ -21,14 +21,44 @@ module.exports = {
       "val": "0",
       "type": "number",
       "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydnotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "zqydnotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "zqydMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
     }],
 
     "favIcon": "mdi-star-outline",
     "favIconColor": "grey",
-    "datas": [
+    "datas": [{
       "key": "zqydbody",
       "val": "p=O2JgR8oZr6IU%3DBty5sXh9aR9qYRLTZOTU178MyLkRYeBiZOP65oO7v9djzqrwX3cYkbFrOS1HkE-vALcr9blKVJSwnFzZzq_b99O8IM89Tde_344gaYfXd5XSwrEead1RLrMyaH76p4b6JMV45F_ZmBpA8Ybpw32lqHdy-rne116FLbSf_P6072B4iST3Wk_uWUDMlNAE4BAEGSXXtyUrnDWB49Y0R5hpEutzeJDl0CL1eTZVDnL9E7_MCFfBLlnOA77EGXQRYYKbtN3P--PedCT6opIAYWZ69J9hfmonZ-5-XxADxTZR905-PQK97qV8EfTMWcZxrjD5elorcGMKEUcMcIaU0n1tFw7dojipxJ6AVEukfuBGHzi7HcoCUDB8jKtxa4M3UM0gfd97T1TqoJlyYGSEBUONaqY55EEsO4v30quimPbyWsEwDgC6Zt8sz-x_kjJLPWaS9bdTp3OcaXQUNexJnTUM1e8JOqULHXjDpeSYaBvGEnz6NZA26ljYBkV9lKfg2DSKN5DRfhE6SGixKN-IRfeJtyATN0iuAJmqM6R_ME56vjQ8dRe0knkHbSN8uQTuyzd4vZ2qpB0eve28WxGtt0OLWuG3Of7slC0tgLg0VrBL2jIfetz6gkFudK3Pjhq_Smu-pcu4s3dABcEeOjzIBTuk8JXSvzREwNXmB88jWjFWQg_py8XYGuoHEaQx2PejlS4-6MQPjnxRMeHsu6Z7sPYofE65YNICiEtFSv2ZcMaE82c36taJVtI-8K5vf3m5k4aUdntpJGdd6keG4A7SLp5Qo3raXgbe21vq7p9d6dYSbW94cMqMrgY4FS8HgJWkrPvvmFPYyLMLuygTL5IpdtGVIUMDytU0sCDH4QWYsY_saXlMEdmLe0MeUItKXPgYhnljMe6IClcoBnbchRYuWi4ohJOFzb2vWfPL3sfDha6Zd42jPk9ItFqhhpj9fdgWBrmTRD4k11KHs3Z0e6VJFUkdMx4pTL4lF3cf-_5194Wj1PETSVllxbZUgdnG3Xa3sBguYzA5KxxQT1Bf0UaQlYgzDIdKPo9jFyX-AcjSj5ZS5xPN-tfP1x5B2cQ2hri8aSxpfvmSULZmCUEVGZsW6WQGPH2xBpi4w2fgMBxctD3IjFQ%3D"
-  ],
+    }
     "sessions": [],
     "isLoaded": true
   }
